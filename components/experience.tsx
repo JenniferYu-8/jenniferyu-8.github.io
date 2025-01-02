fe20df3a0c2aa34734ed94735ce95aba65ca13bc
Jennifer-Yu8 <jenniferyu.on@gmail.com>
2025-01-01 23:32:10 -0500

    fix experience nav

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,7 +9,7 @@ import { useSectionInView } from '@/lib/hooks';
 import { useTheme } from '@/context/theme-context';
 
 export default function Experience() {
-    const { ref } = useSectionInView("Experience", 0.5);
+    const { ref } = useSectionInView("Experience", 0.3);
     const { theme } = useTheme();
   return (
     <section ref={ref} className="scroll-mt-28 mb-28 sm:mb-40 w-full" id="experience">
